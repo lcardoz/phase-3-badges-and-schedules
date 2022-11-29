e9e64b371df85a59e75e48311a8ef392cd1a2c12
Leah Cardoz <leahcardoz@gmail.com>
2022-11-29 16:29:08 -0500

    commit tree 013f260d020412b2c3ce8af2330cdac7a35a8d4d

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker (name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (names)
+# takes an array of names as an argument and returns an array of badge messages
+    names.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms (names)
+    names.map.with_index(1) do |name, room|
+        "Hello, #{name}! You'll be assigned to room #{room}!"
+    end
+end
+
+def printer (names)
+    batch_badge_creator(names).each do |badge|
+        puts badge
+    end
+
+    assign_rooms(names).each do |assignment|
+        puts assignment
+    end
+end
